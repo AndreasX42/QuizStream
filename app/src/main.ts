bbb0173d01457ef5ddaf250c36e011da624d44fb
AndreasX42 <andreas.pangerl.42@gmail.com>
2024-08-11 11:21:18 -0400

    restructure repo

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter([
+    {
+      path:
+    }
+  ])]
+})
   .catch((err) => console.error(err));
