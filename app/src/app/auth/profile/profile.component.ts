172c7d09ae7c2f131e0b51379d5e346854b82d17
AndreasX42 <andreas.pangerl.42@gmail.com>
2024-08-31 14:52:22 -0400

    fix bug in session expiry logic

--- app/src/app/auth/profile/profile.component.ts
+++ app/src/app/auth/profile/profile.component.ts
@@ -36,25 +36,28 @@ export class ProfileComponent implements OnInit {
     const token = this.authService.getJwtToken();
     if (token) {
       const decodedToken = this.authService.decodeToken();
+
       const expiryTime = decodedToken.exp * 1000;
 
       interval(1000).subscribe(() => {
         const currentTime = Date.now();
         const timeLeft = expiryTime - currentTime;
 
-        if (timeLeft <= 0) {
+        if (timeLeft > 0) {
+          this.sessionTimeLeft.set(this.formatTimeLeft(timeLeft));
+        } else {
           this.sessionTimeLeft.set('Session expired');
           this.authService.logout();
-        } else {
-          this.sessionTimeLeft.set(this.formatTimeLeft(timeLeft));
         }
       });
     }
   }
 
   formatTimeLeft(timeLeft: number): string {
+    const hours = Math.floor((timeLeft / 1000 / 60 / 60) % 24);
     const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
     const seconds = Math.floor((timeLeft / 1000) % 60);
-    return `${minutes}m ${seconds}s`;
+
+    return `${hours}h ${minutes}m ${seconds}s`;
   }
 }
